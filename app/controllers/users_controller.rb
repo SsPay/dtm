69b7c194cc145448f5ad5343c7a40135c282eae5
SsPay <sapsay.99@mail.ru>
2020-08-10 16:02:57 +0300

    Add favourite team

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,20 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show]
+  before_action :set_user, only: %i[show edit update]
 
   def show
     @teams = @user.teams
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: "User #{@user.nickname} has been edited"
+    else
+      render :edit
+    end
   end
 
   def index
@@ -13,6 +25,10 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.permit(:favourite_team_id)
   end
 
 end
