886aca3b1b5a346c0509b5bae2ea2346918750dd
SsPay <sapsay.99@mail.ru>
2020-10-09 15:19:08 +0300

    add fb oauth

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -14,4 +14,18 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     redirect_to after_sign_in_path_for(user)
   end
+
+  def facebook
+    auth = request.env["omniauth.auth"]
+    user = User.where(provider: auth["provider"], uid: auth["uid"])
+            .first_or_initialize(email: auth["info"]["email"], name: auth["info"]["firts_name"])
+    user.password = SecureRandom.urlsafe_base64
+    user.save!
+
+    user.remember_me = true
+    sign_in(:user, user)
+
+    redirect_to after_sign_in_path_for(user)
+  end
+
 end
